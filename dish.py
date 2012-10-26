14f533342067bd13b1b8a399315e63448af220d5
張ジョンホ <jeongho.jang@nhn.com>
2012-10-26 18:51:15 +0900

    indented

--- dish.py
+++ dish.py
@@ -17,7 +17,7 @@ class Reader(threading.Thread):
 		super(Reader, self).__init__()
 		self.hostname = hostname
 		self.stream = stream
-	
+
 	def run(self):
 		while True:
 			line = self.stream.readline()
@@ -26,46 +26,46 @@ class Reader(threading.Thread):
 			print self.hostname, line.rstrip()
 
 def executeRemoteCommand(host, command):
-        popen = subprocess.Popen([rsh, host, command], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        return popen
+	popen = subprocess.Popen([rsh, host, command], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	return popen
 
 def waitForTermination(popens):
-        newPopens = []
-        for popen in popens:
-                if popen.poll() == None:
-                        popens.append(popen)
-                else:
-                        print "pid %d returned %d" % (popen.pid, popen.returncode)
-        return newPopens
+	newPopens = []
+	for popen in popens:
+		if popen.poll() == None:
+			popens.append(popen)
+		else:
+			print "pid %d returned %d" % (popen.pid, popen.returncode)
+	return newPopens
 
 def resolveHostRange(args):
-        newArgs = []
-        newArgs.append(hostRangeResolver)
-        newArgs.extend(args)
-        #processHandle = subprocess.Popen(newArgs, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-        processHandle = subprocess.Popen(newArgs, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        (out, err) = processHandle.communicate()
-        return out.split()
+	newArgs = []
+	newArgs.append(hostRangeResolver)
+	newArgs.extend(args)
+	#processHandle = subprocess.Popen(newArgs, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+	processHandle = subprocess.Popen(newArgs, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	(out, err) = processHandle.communicate()
+	return out.split()
 
 if(__name__ == "__main__"):
-        usage = "%prog hostname command"
-        optParser = OptionParser(usage)
-        optParser.add_option("-c", dest="command", help="command to execute on remote machines", default="ls")
-        (options, args) = optParser.parse_args()
-        hosts = resolveHostRange(args)
-        #print hosts
-        popens = []
-        timeStart = time.time()
-        for host in hosts:
-                popen = executeRemoteCommand(host, options.command)
-                popens.append(popen)
-				stdoutReader = Reader(host, popen.stdout)
-				stdoutReader.start()
-				stderrReader = Reader(host, popen.stderr)
-				stderrReader.start()
-        while len(popens) > 0:
-                popens = waitForTermination(popens)
-                time.sleep(0.1)
-        print "elapsed %.3f ms" % ((time.time() - timeStart) * 1000)
+	usage = "%prog hostname command"
+	optParser = OptionParser(usage)
+	optParser.add_option("-c", dest="command", help="command to execute on remote machines", default="ls")
+	(options, args) = optParser.parse_args()
+	hosts = resolveHostRange(args)
+	#print hosts
+	popens = []
+	timeStart = time.time()
+	for host in hosts:
+		popen = executeRemoteCommand(host, options.command)
+		popens.append(popen)
+		stdoutReader = Reader(host, popen.stdout)
+		stdoutReader.start()
+		stderrReader = Reader(host, popen.stderr)
+		stderrReader.start()
+	while len(popens) > 0:
+		popens = waitForTermination(popens)
+		time.sleep(0.1)
+	print "elapsed %.3f ms" % ((time.time() - timeStart) * 1000)
 
 
