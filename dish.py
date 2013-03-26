5b1e0bfc3582029233cea8bb99070cbf5aa52051
張ジョンホ <jeongho.jang@nhn.com>
2013-03-26 15:23:02 +0900

    Add parse_host_range function to dish.py

--- dish.py
+++ dish.py
@@ -12,6 +12,7 @@ rsh = "/usr/kerberos/bin/rsh"
 
 hostRangeResolver = "./host_range_resolver.py"
 
+
 class Reader(threading.Thread):
 	def __init__(self, hostname, stream):
 		super(Reader, self).__init__()
@@ -38,6 +39,47 @@ def waitForTermination(popens):
 			print "pid %d returned %d" % (popen.pid, popen.returncode)
 	return newPopens
 
+#
+# 'foo01~foo03,foo05,foo10' resolved to 'foo01 foo02 foo03 foo05 foo10'
+#
+def parse_hostname(hostname):
+    final = len(hostname)
+    for index in range(len(hostname)):
+        if hostname[index:].isdigit():
+            final = index
+            break
+    return hostname[:final], hostname[final:]
+
+def resolve_host_range(args):
+    first = True
+    result = []
+    
+    for arg in args:
+        tokens = arg.split(',')
+        for token in tokens:
+            hosts = token.split('~')
+            hosts_len =  len(hosts)
+            if first == True:
+                first = False
+            if hosts_len == 1:
+                result.append(hosts[0])
+            elif hosts_len == 2:
+                # hosts should share non-numeric substring
+                prefix0, postfix0 = parse_hostname(hosts[0])
+                prefix1, postfix1 = parse_hostname(hosts[1])
+                if prefix0 != prefix1:
+                    # TODO: exception
+                    print >> sys.stderr, 'invalid host range string'
+                    sys.exit(1)
+                for index in range(int(postfix0), int(postfix1) + 1):
+                    result.append(prefix0 + str(index))
+            else:
+                # TODO: exception
+                print >> sys.stderr, 'invalid host range string'
+                sys.exit(1)
+
+    return result
+
 def resolveHostRange(args):
 	newArgs = []
 	newArgs.append(hostRangeResolver)
@@ -52,8 +94,9 @@ if(__name__ == "__main__"):
 	optParser = OptionParser(usage)
 	optParser.add_option("-c", dest="command", help="command to execute on remote machines", default="ls")
 	(options, args) = optParser.parse_args()
-	hosts = resolveHostRange(args)
+	hosts = resolve_host_range(args)
 	#print hosts
+	#sys.exit(1)
 	popens = []
 	timeStart = time.time()
 	for host in hosts:
